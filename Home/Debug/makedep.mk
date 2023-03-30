81553461433947c6e43850afd1187bded134691e
KhloodElhossiny <47139708+khloodelhossiny@users.noreply.github.com>
2023-03-30 23:12:50 +0200

    Add files via upload

---
+++ Home/Debug/makedep.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+ADC.c
+
+heap_2.c
+
+keypad.c
+
+lcd.c
+
+list.c
+
+main.c
+
+port.c
+
+PWM.c
+
+queue.c
+
+tasks.c
+
