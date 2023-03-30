dad3cd4a602e2c20896693478b6e5c3c1472e0c7
KhloodElhossiny <47139708+khloodelhossiny@users.noreply.github.com>
2023-03-30 23:21:52 +0200

    Add files via upload

---
+++ Enter_home/Debug/makedep.mk
@@ -0,0 +1,16 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+eeprom.c
+
+keypad.c
+
+lcd.c
+
+main.c
+
+PWM.c
+
+smart_home.c
+
